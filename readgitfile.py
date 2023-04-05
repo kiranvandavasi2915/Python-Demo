ccdf4f8b234e95a8d1581e47402810dcd4e6f33d
kiranvandavasi2915 <129794311+kiranvandavasi2915@users.noreply.github.com>
2023-04-05 14:15:07 +0100

    Update readgitfile.py
    Comments added to the files

--- readgitfile.py
+++ readgitfile.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+#########Reads Data from GIT##########
+
 url_github = 'https://raw.githubusercontent.com/kiranvandavasi2915/raw/main/Customers.csv?raw=true'
 pd.set_option('display.max_columns', None)
 pd_df = pd.read_csv(url_github, index_col=0)
